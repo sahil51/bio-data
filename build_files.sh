b4e5b06376637150112a6d28e9d2581859875ee2
Sahil Rajput <sahilrajputofficial0@gmail.com>
2025-02-19 00:58:26 +0530

    Fixed Vercel deployment issue

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
+
+# Install pip if not available
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 python3 get-pip.py
+
+# Install dependencies
+pip install --upgrade pip
 pip install -r requirements.txt
+
+# Collect static files
 python3 manage.py collectstatic --noinput
