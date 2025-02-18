69496020ee152e9a85e7f76341adea8ab46995e4
Sahil Rajput <sahilrajputofficial0@gmail.com>
2025-02-19 00:49:23 +0530

    Fixed deployment issues

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,5 @@
+#!/bin/bash
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+python3 get-pip.py
 pip install -r requirements.txt
-python3.9 manage.py collectstatic
+python3 manage.py collectstatic --noinput
