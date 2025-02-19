17273302a7d4ace11199030833829c65e549d664
Sahil Rajput <sahilrajputofficial0@gmail.com>
2025-02-19 23:28:09 +0530

    Fixed missing dependencies for Vercel deployment

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,14 @@
-# build_files.sh
+#!/bin/bash
+
+# Ensure pip is installed
+python -m ensurepip --default-pip
+
+# Upgrade pip and install dependencies
+python -m pip install --upgrade pip
 pip install -r requirements.txt
 
-# make migrations
-python3.9 manage.py migrate 
-python3.9 manage.py collectstatic
+# Run migrations (optional, if needed)
+python manage.py migrate
+
+# Collect static files
+python manage.py collectstatic --noinput
